174ab3625d608d9ebe4953e56b828c3eb4bbcdd0
pabloperezarcos <pabloperezarcos@gmail.com>
2025-01-10 01:30:16 -0300

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,46 +1,14 @@
-import { Component, OnInit } from '@angular/core';
-import { MsalService } from '@azure/msal-angular';
-import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
-  imports: [CommonModule],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'Front-Duoc-Azure';
-  isLoggedIn = false;
 
-  constructor(private msalService: MsalService) { }
-
-  ngOnInit(): void {
-    const account = this.msalService.instance.getActiveAccount();
-    this.isLoggedIn = !!account;
-  }
-
-  login() {
-    this.msalService.loginPopup().subscribe({
-      next: (result) => {
-        console.log('Login success:', result);
-        this.isLoggedIn = true;
-      },
-      error: (error) => {
-        console.error('Login error:', error);
-        this.isLoggedIn = false;
-      },
-    });
-  }
-
-  logout() {
-    this.msalService.logoutPopup().subscribe({
-      next: () => {
-        console.log('Logout success');
-        this.isLoggedIn = false;
-      },
-      error: (error) => {
-        console.error('Logout error:', error);
-      },
-    });
-  }
 }
